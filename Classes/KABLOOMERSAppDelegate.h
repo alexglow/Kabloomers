813c3f446f657351cbaaa400a4ac010c06551a92
Alex 'Merlin' Glowaski <alex.glowaski@gmail.com>
2011-11-01 01:36:58 -0700

    here we gooooo

---
+++ Classes/KABLOOMERSAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  KABLOOMERSAppDelegate.h
+//  KABLOOMERS
+//
+//  Created by alex on 10/29/11.
+//  Copyright 2011 Misk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KABLOOMERSAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
